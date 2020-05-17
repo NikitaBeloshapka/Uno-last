4a46767e6e0b997b3796fd57a777601fa86c4417
NikitaBeloshapka <65022653+NikitaBeloshapka@users.noreply.github.com>
2020-05-17 14:16:55 +0300

    Update Form1.cs

--- Uno last/Form1.cs
+++ Uno last/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,17 @@ namespace Uno_last
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GraphicUno game = new GraphicUno( new GraphicCardSet(panel1, 52),new Player("Nikita"),new Player("Misha"),panel1,panel2,panel3);
+            game.RegisterHandler(Sho);
+            game.Start();
+        }
+        private void Sho(string message)
+        {
+            MessageBox.Show(message);
         }
     }
 }
