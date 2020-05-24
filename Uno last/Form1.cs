6637d007f7ebfffcf4886f765ff5fb01eba58b76
Никита Белошапка <beliykharkov.n@gmail.com>
2020-05-24 14:12:11 +0300

    dbggtde

--- Uno last/Form1.cs
+++ Uno last/Form1.cs
@@ -8,13 +8,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Uno_last
+namespace Сards
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GraphicUno game = new GraphicUno( new GraphicCardSet(panel1, 52),new Player("Nikita"),new Player("Misha"),panel1,panel2,panel3);
+            game.RegisterHandler(Sho);
+            game.Start();
+        }
+        private void Sho(string message)
+        {
+            MessageBox.Show(message);
         }
     }
 }
