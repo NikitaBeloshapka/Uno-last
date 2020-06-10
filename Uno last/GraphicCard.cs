992ae3181db4e327749d29515c2f0e41e8766e28
NikitaBeloshapka <65022653+NikitaBeloshapka@users.noreply.github.com>
2020-06-10 11:25:14 +0300

    Update GraphicCard.cs

--- Uno last/GraphicCard.cs
+++ Uno last/GraphicCard.cs
@@ -1,24 +1,58 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Сards;
 
-namespace Сards
+namespace Cards
 {
-    public class GraphicCard:Card
+    class GraphicCard : Card
     {
-        public GraphicCard(CardColour colour, KindsOfCards kinds) : base(colour, kinds) {  }
-
         public PictureBox Pb { get; set; }
+        public bool Opened
+        {
+            get
+            {
+                return opened;
+            }
+            set
+            {
+                opened = value;
+                Pb.Image = opened ? Image.FromFile(fileName) : Image.FromFile(imageShirtPath);
+            }
+        }
+
+        private bool opened;
+        private readonly string imageShirtPath = Application.StartupPath + @"\images\shirt.jpg";
+        private readonly string fileName;
+
+        public GraphicCard(KindsOfCards kinds, CardColour colour, PictureBox pb, bool opened = true) : base(colour,kinds)
+        {
+            Pb = pb;
+            Pb.SizeMode = PictureBoxSizeMode.Zoom;
+            Pb.Visible = false;
+            fileName = Application.StartupPath + @"\images\" + this.ToString() + ".png";
+            Opened = opened;
+        }
+
+        public GraphicCard(KindsOfCards kinds, CardColour colour) : this(kinds, colour, new PictureBox()) { }
+
+        public GraphicCard(CardColour colour, KindsOfCards kinds) : base(colour, kinds)
+        {
+        }
 
         public override void Show()
         {
-            var exePath = AppDomain.CurrentDomain.BaseDirectory;//path to exe file
-            var path1 = Path.Combine(exePath, @"images/" + Kinds.ToString() + " " + Colour.ToString()+".png");
-            Pb.ImageLocation = path1;
+            Pb.Visible = true;
         }
+
+        public override string ToString()
+        {
+            return String.Format($"{Kinds}s {Colour}");
+        }
+
     }
 }
